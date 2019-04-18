1fd25b8b7034f656cd72ee6dad0df675e048778c
OwinBlazer <33065589+OwinBlazer@users.noreply.github.com>
2019-04-18 19:34:43 +0700

    Spawn Table and Spawn QuantityTracking
    Spawning Table to control the chances of a random spawn as well as to limit its total spawn has been implemented. Spawn Table Flag added to every object which is counted as one instance of the table.

---
+++ ProjectI-OmniVR/Assets/_Owin/OmniVR/Script/OW_SpawnTable.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class SpawnEntry {
+    public EnemyPool enemyPool;
+    public float spawnChance;
+    public int spawnLimit;
+    public int spawnCurr;
+}
+
+public class OW_SpawnTable : MonoBehaviour {
+    [SerializeField] SpawnEntry[] spawnList;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public GameObject SpawnEnemyRandom(Transform position)
+    {
+        float total = 0;
+        foreach(SpawnEntry entry in spawnList)
+        {
+            total += entry.spawnChance;
+        }
+
+        float rng = Random.Range(0, total);
+
+        int i = 0;
+        while (rng > spawnList[i].spawnChance)
+        {
+            rng -= spawnList[i].spawnChance;
+            i++;
+        }
+        GameObject tempObject;
+        if (spawnList[i].spawnLimit > 0)
+        {
+            if (spawnList[i].spawnCurr < spawnList[i].spawnLimit)
+            {
+                spawnList[i].spawnCurr++;
+                tempObject = spawnList[i].enemyPool.IssueFromPool(position);
+                tempObject.GetComponent<OW_SpawnTableFlag>().AssignIndex(i, this);
+                return tempObject;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            tempObject = spawnList[i].enemyPool.IssueFromPool(position);
+            tempObject.GetComponent<OW_SpawnTableFlag>().AssignIndex(i,this);
+            return tempObject;
+        }
+    }
+    public GameObject SpawnEnemyType(int ID, Transform position)
+    {
+        int index = -1;
+        for(int i = 0; i < spawnList.Length; i++)
+        {
+            if (spawnList[i].enemyPool.GetID() == ID)
+            {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1)
+        {
+            if (spawnList[index].spawnLimit > 0)
+            {
+                spawnList[index].spawnCurr++;
+                GameObject tempObject = spawnList[index].enemyPool.IssueFromPool(position);
+                tempObject.GetComponent<OW_SpawnTableFlag>().AssignIndex(index, this);
+                return tempObject;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public void ReportInactive(int index)
+    {
+        spawnList[index].spawnCurr--;
+    }
+}
---
+++ ProjectI-OmniVR/Assets/_Owin/OmniVR/Script/OW_SpawnTableFlag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OW_SpawnTableFlag : MonoBehaviour {
+    private int assignedIndex;
+    private OW_SpawnTable spawnTable;
+    public void AssignIndex(int index,OW_SpawnTable referenceTable)
+    {
+        assignedIndex = index;
+        spawnTable = referenceTable;
+    }
+    private void OnDisable()
+    {
+        spawnTable.ReportInactive(assignedIndex);
+    }
+}
